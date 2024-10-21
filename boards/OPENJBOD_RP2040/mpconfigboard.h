25a5edee57beb8adfcb6add3f449a3b98150f9af
TheGuyDanish <5776313+TheGuyDanish@users.noreply.github.com>
2024-10-21 08:33:42 +0200

    disable USB serial

--- boards/OPENJBOD_RP2040/mpconfigboard.h
+++ boards/OPENJBOD_RP2040/mpconfigboard.h
@@ -1,7 +1,11 @@
 // Board and hardware specific configuration
 #define MICROPY_HW_BOARD_NAME                   "OpenJBOD RP2040"
+// Disable USB serial console for production
+#define MICROPY_HW_USB_CDC			(0)
+#define MICROPY_HW_ENABLE_UART_REPL		(1)
 // Reserve 1MB for MPy firmware
 #define MICROPY_HW_FLASH_STORAGE_BYTES          (PICO_FLASH_SIZE_BYTES - (1 * 1024 * 1024))
+// Defaults for networking
 #define MICROPY_PY_NETWORK			(1)
 #define MICROPY_PY_NETWORK_HOISTNAME_DEFAULT	"openjbod"
 #define MICROPY_HW_WIZNET_SPI_ID		(0)
